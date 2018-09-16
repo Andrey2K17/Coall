54aab2a42400f53d914caf911873aa3eea22d945
Andrey2K17 <nba.grinin.96@gmail.com>
2018-09-16 19:22:33 +0700

    Подключенил базу, вывел содержимое таблиц в 2 alertDialog, не связал таблицы

--- app/src/main/java/ag/pg13/coal/DBHelper/DBHelper.kt
+++ app/src/main/java/ag/pg13/coal/DBHelper/DBHelper.kt
@@ -1,10 +1,14 @@
 package ag.pg13.coal.DBHelper
 
+import ag.pg13.coal.Model.Mark
 import ag.pg13.coal.Model.Provider
 import android.content.Context
+import android.database.Cursor
 import android.database.sqlite.SQLiteDatabase
 import android.database.sqlite.SQLiteException
+import android.database.sqlite.SQLiteOpenHelper
 import android.database.sqlite.SQLiteQueryBuilder
+import android.util.Log
 import com.readystatesoftware.sqliteasset.SQLiteAssetHelper
 
 
@@ -17,6 +21,10 @@ class DBHelper(context: Context): SQLiteAssetHelper(context, DB_NAME, null, DB_V
         private val TBL_NAME="provider"
         private val COL_ID="_id"
         private val COL_NAME="name"
+
+        private val TBL_MARK="mark"
+        private val COL_MARK_ID="_id"
+        private val COL_MARK_NAME="mark_name"
     }
 
     //CRUD
@@ -27,20 +35,40 @@ class DBHelper(context: Context): SQLiteAssetHelper(context, DB_NAME, null, DB_V
         val selectQuery = "SELECT * FROM $TBL_NAME"
         val db=this.writableDatabase
         val cursor=db.rawQuery(selectQuery,null)
-        
+
         if(cursor.moveToFirst())
         {
             do{
                 val provider = Provider()
-                provider.id=cursor.getInt(cursor.getColumnIndex(COL_ID))
                 provider.name=cursor.getString(cursor.getColumnIndex(COL_NAME))
-
                 lstProviders.add(provider)
             }while (cursor.moveToNext())
         }
-        //db.close()
+        db.close()
         return lstProviders
     }
+
+    val allMarks:List<Mark>
+    get() {
+        val lstMarks = ArrayList<Mark>()
+        val selectQuery = "SELECT * FROM $TBL_MARK"
+        val db=this.writableDatabase
+        val cursor=db.rawQuery(selectQuery,null)
+
+        if(cursor.moveToFirst())
+        {
+            do{
+                val mark = Mark()
+                mark.name=cursor.getString(cursor.getColumnIndex(COL_MARK_NAME))
+                lstMarks.add(mark)
+            }while (cursor.moveToNext())
+        }
+        db.close()
+        return lstMarks
+    }
+
+
+
 
 
 }
---
+++ app/src/main/java/ag/pg13/coal/Model/Mark.kt
@@ -0,0 +1,14 @@
+package ag.pg13.coal.Model
+
+class Mark {
+    var id: Int=0
+    var name:String?=null
+
+    constructor(){}
+
+    constructor(id: Int, name: String?) {
+        this.id = id
+        this.name = name
+    }
+
+}
